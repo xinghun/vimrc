a55dafc06ded33041dd667e3e5b66ff5ac8c4af1
xinghun <215796979@qq.com>
2020-07-17 16:33:23 +0800

    vim

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ set -e
 cd ~/.vim_runtime
 
 echo '"set runtimepath+=~/.vim_runtime
-source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/basic.vim
-"source ~/.vim_runtime/vimrcs/filetypes.vim
-"source ~/.vim_runtime/vimrcs/extended.vim
+source ~/.vim_runtime/vimrcs/plugins_config.vim
 try
 source ~/.vim_runtime/vimrcs/my_configs.vim
 catch
